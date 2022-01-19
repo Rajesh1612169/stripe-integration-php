cd4faf4aa75d841912e21894baa4d4d35ba3e12b
Rajesh1612169 <67611897+Rajesh1612169@users.noreply.github.com>
2022-01-19 18:01:31 +0500

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 require('stripe-php-master/init.php');
 
-$publishable_key = "pk_test_51KHjnXLFnwqQ9rKKHWy7te9qTt3XPil4IhqzXcndMpcWh9MLbPf0Bo8vFp6KCXL2acHHkRuTAUQNtYI8KTrY8g1O00jxWNAioy";
-$secret_key = "sk_test_51KHjnXLFnwqQ9rKKaanuv15jH1Q3ODFA7Tsy4WIU01g6S2UrE0OdwIngyTCqvlb8ltwrsCX9UKvCgowl0TrqdrRf00YC8F9MrF";
+$publishable_key = "PUBLISAHBLE-KEY";
+$secret_key = "SECRET-KEY";
 
 \Stripe\Stripe::setApiKey($secret_key);
 
